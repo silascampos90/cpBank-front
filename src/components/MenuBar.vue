580520008bc72f6a1bce3a19efaebf3c55175007
Silas Silva Campos <sscampos@teltelematica.com.br>
2020-10-22 16:49:53 -0300

    bootstrap

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,8 +1,23 @@
 <template>
-  <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-  </div>
+  <div>
+  <b-card title="Card Title" no-body>
+    <b-card-header header-tag="nav">
+      <b-nav card-header tabs>
+        <b-nav-item active>Active</b-nav-item>
+        <b-nav-item>Inactive</b-nav-item>
+        <b-nav-item disabled>Disabled</b-nav-item>
+      </b-nav>
+    </b-card-header>
+
+    <b-card-body class="text-center">
+      <b-card-text>
+        With supporting text below as a natural lead-in to additional content.
+      </b-card-text>
+
+      <b-button variant="primary">Go somewhere</b-button>
+    </b-card-body>
+  </b-card>
+</div>
 </template>
 <script>
 export default {
