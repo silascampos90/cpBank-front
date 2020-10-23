b92c44268cb9135f23bff042ce8a1b5f71337f90
Silas Silva Campos <sscampos@teltelematica.com.br>
2020-10-23 15:36:38 -0300

    final msg

--- src/global.js
+++ src/global.js
@@ -4,8 +4,18 @@ export const userKey = '__knowledge_user'
 export const baseApiUrl = 'http://localhost:8000/api'
 
 export function showError(e) {
-    if (e && e.response && e.response.data) {
-        Vue.toasted.global.defaultError({ msg: e.response.data })
+    if (e && e.response && e.response.data.errors) {
+        var message;
+        if(e.response.data.errors.password){
+            message = e.response.data.errors.password[0]
+        }
+        if(e.response.data.errors.generic){
+            message = e.response.data.errors.generic[0]
+        }
+        if(e.response.data.errors.cpf){
+            message = e.response.data.errors.cpf[0]
+        }
+        Vue.toasted.global.defaultError({ msg: message })
     } else if (typeof e === 'string') {
         Vue.toasted.global.defaultError({ msg: e })
     } else {
